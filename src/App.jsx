d824b445fb1ced0ace08a04a42fc0ee39564daca
21MoududAhammed <21moudud@gmail.com>
2024-02-11 19:02:36 +0600

    fix the problem by adding roomId as a dependency of an Effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
-export default function App(){
-    return (
-        <h2>Hello World!</h2>
-    );
+import { useState, useEffect } from 'react';
+import { createConnection } from './index';
+
+const serverUrl = 'https://localhost:1234';
+
+function ChatRoom({ roomId }) {
+  useEffect(() => {
+    const connection = createConnection(serverUrl, roomId);
+    connection.connect();
+    return () => connection.disconnect();
+  }, [roomId]); // <-- Fix the mistake here!
+  //It has fixed by adding roomId as dependency.
+  return <h1>Welcome to the {roomId} room!</h1>;
+}
+
+export default function App() {
+  const [roomId, setRoomId] = useState('general');
+  return (
+    <>
+      <label>
+        Choose the chat room:{' '}
+        <select
+          value={roomId}
+          onChange={e => setRoomId(e.target.value)}
+        >
+          <option value="general">general</option>
+          <option value="travel">travel</option>
+          <option value="music">music</option>
+        </select>
+      </label>
+      <hr />
+      <ChatRoom roomId={roomId} />
+    </>
+  );
 }
